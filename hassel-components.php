02f1a391dbaadf4532240c9ccecf752ee0d20132
BericaEditrice <webmaster@bericaeditrice.it>
2026-02-19 11:35:32 +0100

    Update hassel-components.php

--- hassel-components.php
+++ hassel-components.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH'))
     exit;
 
 // === Costanti ===
-define('HASSEL_COMPONENTS_VERSION', '1.3.28');
+define('HASSEL_COMPONENTS_VERSION', '1.3.29');
 define('HASSEL_COMPONENTS_PATH', plugin_dir_path(__FILE__));
 define('HASSEL_COMPONENTS_URL', plugin_dir_url(__FILE__));
 
